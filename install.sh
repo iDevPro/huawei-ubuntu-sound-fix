93bccfc8d94dbee6cab7dcdca6b25fd712d48b78
Субач Павел <psubach@rbc.ru>
2024-03-10 23:40:49 +0300

    Update installation and deinstallation procedures

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-installService() {
+install_huawei_soundcard_service() {
     echo "Copying files..."
     cp huawei-soundcard-headphones-monitor.sh /usr/local/bin/
     cp huawei-soundcard-headphones-monitor.service /etc/systemd/system/
@@ -17,33 +17,33 @@ installService() {
     echo "Complete!"
 }
 
-INSTALL=$(declare -f installService)
+export -f install_huawei_soundcard_service
 
 if command -v apt &>/dev/null; then
     echo "Using apt to install dependencies..."
     sudo apt update
     sudo apt install -y alsa-tools alsa-utils
-    sudo bash -c "$INSTALL"
+    sudo install_huawei_soundcard_service
 elif 
     command -v pacman &>/dev/null; then
     echo "Using pacman to install dependencies..."
     sudo pacman -Sy alsa-tools alsa-utils --noconfirm
-    sudo bash -c "$INSTALL"
+    sudo install_huawei_soundcard_service
 elif
     command -v eopkg &>/dev/null; then
     echo "Using eopkg to install dependencies..."
     sudo eopkg up
     sudo eopkg it alsa-tools alsa-utils -y
-    sudo bash -c "$INSTALL"
+    sudo install_huawei_soundcard_service
 elif 
     command -v dnf &>/dev/null; then
     echo "Using dnf to install dependencies..."
     sudo dnf install -y alsa-tools alsa-utils
-    sudo bash -c "$INSTALL"
+    sudo install_huawei_soundcard_service
 elif
     command -v apt-get &>/dev/null && grep -q "ID=altlinux" /etc/os-release; then
     echo "Using apt-get in AltLinux to install dependencies..."
-    su -l -c "apt-get update && apt-get dist-upgrade && apt-get install alsa-tools alsa-utils && $INSTALL"
+    su -c "apt-get update && apt-get dist-upgrade && apt-get install alsa-tools alsa-utils && install_huawei_soundcard_service"
 else
     echo "Neither apt, pacman, eopkg, nor dnf found. Cannot install dependencies."
     exit 1
--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,22 @@
-echo "Stopping daemon..."
-sudo systemctl stop huawei-soundcard-headphones-monitor.service
+#!/bin/bash
 
-echo "Removing program..."
-sudo rm /usr/local/bin/huawei-soundcard-headphones-monitor.sh
+uninstall_huawei_soundcard_service() {
+    echo "Stopping daemon..."
+    systemctl stop huawei-soundcard-headphones-monitor.service
 
-echo "Removing service..."
-sudo rm /etc/systemd/system/huawei-soundcard-headphones-monitor.service
+    echo "Removing program..."
+    rm /usr/local/bin/huawei-soundcard-headphones-monitor.sh
 
-echo "Uninstalled. Goodbye 😿"
+    echo "Removing service..."
+    rm /etc/systemd/system/huawei-soundcard-headphones-monitor.service
+
+    echo "Uninstalled. Goodbye 😿"
+}
+
+export -f uninstall_huawei_soundcard_service
+
+if grep -q "ID=altlinux" /etc/os-release; then
+    su -c "uninstall_huawei_soundcard_service"
+else
+    sudo uninstall_huawei_soundcard_service
+fi
